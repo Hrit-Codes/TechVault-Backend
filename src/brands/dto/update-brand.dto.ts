e5acfaf48eb62bb0026ae1f71710700fbc1f3ebd
Hrit Amatya <privhritamatya@gmail.com>
2026-08-10 18:06:31 +0545

    Updated errors on brands controllers

--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,11 +1,21 @@
-import { IsBoolean, IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
-export class UpdateBrandDto{
-    @IsOptional()
-    @IsString()
-    name?:string
+export class UpdateBrandDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
 
-    @IsOptional()
-    @IsBoolean()
-    isActive?:boolean
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (value === 'true' || value === true) return true;
+    if (value === 'false' || value === false) return false;
+    return value;
+  })
+  @IsBoolean()
+  isActive?: boolean;
+
+  @IsOptional()
+  @IsString()
+  logo?: string;
 }
