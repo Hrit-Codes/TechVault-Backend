2307757fbb4b976121606cb37cd3dedb7a98eabc
Hrit Amatya <privhritamatya@gmail.com>
2026-09-15 17:12:26 +0545

    Implemented redis caching on categories info

--- src/brands/brands.module.ts
+++ src/brands/brands.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { RedisService } from '../redis/redis.service';
 
 @Module({
-  imports:[CloudinaryModule],
+  imports:[CloudinaryModule, RedisService],
   controllers: [BrandsController],
   providers: [BrandsService],
   exports:[BrandsService]
